26b254f111ce21871b18dd21c90144050ec98938
Jocke <joakim03moller@gmail.com>
2026-01-29 09:31:40 +0100

    feat: GET, POST implemented

---
+++ src/services/todoService.ts
@@ -0,0 +1,43 @@
+import type { Todo, NewToDo } from "../types/Todo";
+
+
+const API_URL = "http://localhost:5001/api/todos";
+
+// GET all todos
+export const getTodos = async (): Promise<Todo[]> => {
+    const response = await fetch(API_URL);
+
+    if (!response.ok) {
+        throw new Error('Failed to fetch todos');
+    }
+
+    return response.json();
+};
+
+// GET a specific todo
+export const getTodoById = async (id: string): Promise<Todo> => {
+    const response = await fetch(`${API_URL}/${id}`);
+
+    if (!response.ok) {
+        throw new Error('Failed to fetch todo');
+    }
+
+    return response.json();
+}
+
+// POST a todo
+export const createTodo = async (todo: NewToDo): Promise<Todo> => {
+    const response = await fetch(API_URL, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(todo),
+    });
+
+    if(!response.ok) {
+        throw new Error('Failed to create todo');
+    }
+
+    return response.json();
+};
