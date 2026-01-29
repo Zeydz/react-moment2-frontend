fcd39a11f8b0dd610b5b3a427253fa3e18f07b5a
Jocke <joakim03moller@gmail.com>
2026-01-29 09:43:25 +0100

    style: comments

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ function App() {
 
   const [todos, setTodos] = useState<Todo[]>([]);
 
+  // Fetch todos on component mount
   useEffect(() => {
     const fetchTodos = async () => {
       try {
@@ -22,17 +23,18 @@ function App() {
     fetchTodos();
   }, []);
 
+  // Handle creation of a new todo
   const handleCreate = async (newTodo: NewToDo) => {
     try {
       const createdTodo = await createTodo(newTodo);
+      // Update state with the newly created todo, adding it to the existing list
       setTodos((prevTodos) => [...prevTodos, createdTodo]);
     } catch (error) {
       console.error("Kunde inte skapa todo:", error);
       throw error;
     }
   }
-
-
+  // Render the main application UI.
   return (
     <main className="max-w-xl mx-auto p-6">
       <TodoForm onCreate={handleCreate} />
